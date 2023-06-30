49ced5500b22950a56a44a93f62e1b1143cb8307
root <peter.ramer@marcapo.com>
2023-06-30 10:14:31 +0000

    add springboot adn gradle

---
+++ src/test/java/com/marcapo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcapo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
